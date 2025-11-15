1a4622e24fb1d185e39ad1f96161f134d6f05d27
atharva-git06 <chavanatharva223@gmail.com>
2025-11-14 23:27:55 -0500

    validation updates

--- lib/validation.ts
+++ lib/validation.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const formSchema = z.object({
     title: z.string().min(3, "Title must be at least 3 characters").max(100),
-    description: z.string().min(20, "Description must be at least 20 characters").max(500),
+    description: z.string().min(10, "Description must be at least 20 characters").max(500),
     category: z.string().min(3, "Category must be at least 3 characters").max(20),
     link: z.string().url("Please provide a valid URL"),
     pitch: z.string().min(10, "Pitch must be at least 10 characters")
